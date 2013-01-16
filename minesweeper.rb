942cc808ef6a7b5fd10d43ff41824322a63addfb
Jason Kyle <technique@gmail.com>
2013-01-15 18:11:14 -0800

    almost able to reveal squares successfully, added play loop

--- minesweeper.rb
+++ minesweeper.rb
@@ -2,11 +2,11 @@ require 'debugger'
 
 class Minesweeper
 
+  attr_reader :gameboard
+
   DELTAS = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
 
   def initialize
-    gameboard = build_gameboard(user_settings)
-    print_board(gameboard)
   end
 
   def user_settings
@@ -18,25 +18,39 @@ class Minesweeper
     end
     size
   end
+
+  def play
+    build_gameboard(user_settings)
+    while game_over == false
+      print_board
+      player_move = move
+      check_coordinate(player_move)
+    end
+    "GAME"
+  end
+
+  def game_over
+    false
+  end
+
   # gameboard methods
   def build_gameboard(size)
     n, m = 0
     n, m = 9, 10 if size == 1
     n, m = 16, 40 if size == 2
-    gameboard = Array.new(n) do
+    @gameboard = Array.new(n) do
       Array.new(n) { Square.new }
     end
     #debugger
-    set_bomb(gameboard, m)
-    fringe_sq_iterator(gameboard)
-    gameboard
+    set_bomb(m)
+    fringe_sq_iterator
   end
 
-  def set_bomb(gameboard, bomb_count)
+  def set_bomb(bomb_count)
     bombs = 0
     while true
       return if bombs == bomb_count
-      row = gameboard.sample
+      row = @gameboard.sample
       square = row.sample
       if square.bomb == false
         square.bomb = true
@@ -45,72 +59,97 @@ class Minesweeper
     end
   end
 
-  def print_board(gameboard)
-    gameboard.each do |row|
+  def print_board
+    @gameboard.each do |row|
       row.each do |square|
-        if square.bomb == true
-          print "X"
-          next
-        elsif square.adj_bomb == 0
-          print "*"
-          next
+        if square.revealed == true || square.flagged == true
+          if square.flagged == true
+            print "F"
+            next
+          elsif square.bomb == true
+            print "X"
+            next
+          elsif square.adj_bomb == 0
+            print " "
+          else
+            print "#{square.adj_bomb}"
+          end
         else
-          print square.adj_bomb
+          print "*"
         end
       end
-      puts "\n"
+      print "\n"
     end
   end
 
-  def fringe_sq_iterator(gameboard)
-    gameboard.each_with_index do |row, i|
+  def fringe_sq_iterator
+    @gameboard.each_with_index do |row, i|
       row.each_with_index do |square, j|
         if square.bomb == true
-          adjacents = adjacent_squares(gameboard, [i, j])
+          adjacents = adjacent_squares([i, j])
           adjacents.each do |coordinates|
-            gameboard[coordinates[0]][coordinates[1]].adj_bomb += 1
+            @gameboard[coordinates[0]][coordinates[1]].adj_bomb += 1
           end
         end
       end
     end
   end
 
-  def adjacent_squares(gameboard, coordinates)
+  def adjacent_squares(coordinates)
     adjacents = DELTAS.map do |coord|
       x = coord[0] + coordinates[0]
       y = coord[1] + coordinates[1]
       [x, y]
     end
     selected = adjacents.select do |coord|
-      coord[0] < gameboard.length && coord[0] > 0 && coord[1] < gameboard.length && coord[1] > 0
+      coord[0] < @gameboard.length && coord[0] > 0 && coord[1] < @gameboard.length && coord[1] > 0
     end
-    p selected
     selected
   end
 
   # user methods
 
   def move
-    puts "Type 'R' and your coordinates to reveal (ex: R 4, 5)"
-    puts "Type 'F' and your coordinates to flag"
-    coordinates = gets.chomp
-    if coordinates[:bomb] == true
-      puts "You lost!"
-      return
+    puts "Type 'R' and your coordinates to reveal (ex: R 4 5)"
+    puts "Type 'F' and your coordinates to flag (ex: F 4 5)"
+    move = gets.chomp.split(' ')
+    [move[0].downcase, [move[1].to_i, move[2].to_i]]
+  end
+
+  def check_coordinate(move)
+    type = move[0]
+    coord = move[1]
+    if type == 'r'
+      if @gameboard[coord[0]][coord[1]].bomb == true
+        @gameboard[coord[0]][coord[1]].revealed = true
+        game_over = true
+      else
+        reveal(coord)
+      end
+    elsif type == 'f'
+      @gameboard[coord[0]][coord[1]].flagged = true
     end
   end
 
-  def flag_bomb(coordinates)
-    #just allows a user to see flags of where he thinks bombs are
-  end
+  def reveal(move)
+    #debugger
+    queue = [move]
+    checked = []
 
-  def check_coordinate(coordinates)
-    if coordinates[:bomb] == true
-      puts "You lost!"
-      return
+    while move = queue.shift
+      @gameboard[move[0]][move[1]].revealed = true
+      checked << move
+      to_check = adjacent_squares(move)
+      to_check.each do |coord|
+        if @gameboard[coord[0]][coord[1]].bomb == false || !checked.include?(coord)
+          queue << coord
+        end
+      end
+      p queue
     end
   end
 end
+
 class Square
 
   attr_accessor :bomb, :adj_bomb, :flagged, :revealed
